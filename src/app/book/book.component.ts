2e885c6e3c0bfbe2be8b42e009cfb88267dbeb20
Elisa Boselli <elisaboselli@hotmail.com>
2019-03-04 18:22:28 -0300

    Book list component

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,16 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ApiService } from '../api.service';
+import { MatDialog, MatPaginator, MatSort, MatSortable, MatTableDataSource} from '@angular/material';
+
+import { Book } from '../../../models/Book';
+
 
 @Component({
   selector: 'app-book',
   styleUrls: ['./book.component.css'],
   templateUrl: './book.component.html',
 })
-
 export class BookComponent implements OnInit {
+  displayedColumns: string[] = ['title', 'author', 'publisher'];
+  dataSource;
+  books: Book[];
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  ngOnInit() {
+  constructor(private api: ApiService, public dialog: MatDialog) {
   }
 
+  ngOnInit() {
+    this.api.getBooks()
+    .subscribe((books: Book[]) => {
+      this.books = books;
+      this.dataSource = new MatTableDataSource(books);
+      this.dataSource.paginator = this.paginator;
+      this.sort.sort(<MatSortable>({id: 'title', start: 'asc'}));
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  refresh() {
+    this.api.getBooks().subscribe((books: Book[]) => {
+      this.books = books;
+      this.dataSource.data = books;
+    });
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
